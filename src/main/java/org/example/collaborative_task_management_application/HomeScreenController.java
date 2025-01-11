81058e6e10442457292591774a700d8e5bdcc403
Thrinath008 <148782730+Thrinath008@users.noreply.github.com>
2025-01-11 19:43:22 +0100

    new home screen

---
+++ src/main/java/org/example/collaborative_task_management_application/HomeScreenController.java
@@ -0,0 +1,93 @@
+package org.example.collaborative_task_management_application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class HomeScreenController {
+
+    @FXML
+    private Button exit_button;
+
+    @FXML
+    private AnchorPane kanban_anchorpane;
+
+    @FXML
+    private AnchorPane settings_anchorpane;
+
+    @FXML
+    private Button kanban_button;
+    @FXML
+    private Button logout_button;
+
+    @FXML
+    private AnchorPane messages_anchorpane;
+
+    @FXML
+    private Label messegs_label;
+
+    @FXML
+    private Label name_label;
+
+    @FXML
+    private AnchorPane project_anchorpane;
+
+    @FXML
+    private Button settings_button;
+
+
+    @FXML
+    private void setProject_anchorpane(){
+        project_anchorpane.setVisible(true);
+        messages_anchorpane.setVisible(false);
+        kanban_anchorpane.setVisible(false);
+        settings_anchorpane.setVisible(false);
+    }
+    @FXML
+    private void setMessages_anchorpane(){
+        project_anchorpane.setVisible(false);
+        messages_anchorpane.setVisible(true);
+        kanban_anchorpane.setVisible(false);
+        settings_anchorpane.setVisible(false);
+    }
+    @FXML
+    private void setKanban_anchorpane(){
+        project_anchorpane.setVisible(false);
+        messages_anchorpane.setVisible(false);
+        kanban_anchorpane.setVisible(true);
+        settings_anchorpane.setVisible(false);
+    }
+    @FXML
+    private void setSettings_anchorpane(){
+        project_anchorpane.setVisible(false);
+        messages_anchorpane.setVisible(false);
+        kanban_anchorpane.setVisible(false);
+        settings_anchorpane.setVisible(true);
+    }
+    @FXML
+    public void setlogout_button() throws IOException {
+        logout_button.getScene().getWindow().hide();
+        Parent parent = FXMLLoader.load(getClass().getResource("login-screen.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(parent);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void exit(ActionEvent event){
+        Stage stage = (Stage)exit_button.getScene().getWindow();
+        stage.close();
+
+    }
+
+}
